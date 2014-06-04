6da4b0658bcf2fd4c13569846f6cf31e84981e2c
ChristinaPawlenko <ChristinaPawlenko@yandex.ru>
2014-06-04 09:59:21 +0300

    resolve #3

---
+++ SentenceAnalyzer/WordInfoLoader/HttpLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+namespace WordInfoLoader
+{
+    class HttpLoader
+    {
+        public static string LoadData(string url, string urlReferer, string word)
+        {
+            using (var wb = new WebClient())
+            {
+                wb.Headers.Add("Referer", urlReferer);
+                var data = new NameValueCollection();
+
+                data["word"] = word;
+                var response = wb.UploadValues(url, "POST", data);
+                return Encoding.UTF8.GetString(response);
+            }
+        }
+    }
+}
---
+++ SentenceAnalyzer/WordInfoLoader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace WordInfoLoader
+{
+    class Program
+    {
+        private static string _wordsPath;
+        private static string _outputDir;
+        private static string _url;
+        private static string _urlReferer;
+
+        static void Main(string[] args)
+        {
+            ReadArgs(args);
+
+            var words = File.ReadAllLines(_wordsPath).Select(x => x.Trim()).ToList();
+            for (var i = 0; i < words.Count(); i++)
+            {
+                var word = words[i];
+                var responseText = HttpLoader.LoadData(_url, _urlReferer, word);
+                File.AppendAllText(Path.Combine(_outputDir, word + ".html"), responseText);
+            }
+        }
+
+        private static void ReadArgs(string[] args)
+        {
+            _wordsPath = args[0];
+            _outputDir = args[1];
+            _url = args[2];
+            _urlReferer = args[3];
+        }
+    }
+}
