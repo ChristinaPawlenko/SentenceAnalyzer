c7834430e74903562958a90dc2dfe26857b65f28
ChristinaPawlenko <ChristinaPawlenko@yandex.ru>
2014-06-04 22:14:38 +0300

    Add project WordParser

---
+++ SentenceAnalyzer/WordParser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordParser
+{
+    class Program
+    {
+        private static string _dictionaryPath;
+        private static string _outputDir;
+
+        static void Main(string[] args)
+        {
+            ReadArgs(args);
+
+
+        }
+
+        private static void ReadArgs(string[] args)
+        {
+            _dictionaryPath = args[0];
+            _outputDir = args[1];
+        }
+    }
+}
